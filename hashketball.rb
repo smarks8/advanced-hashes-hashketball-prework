4a59593f7b3a481b50c5773e99ca0d550ba0443f
Stacey  Marks <staceyamarks@gmail.com>
2018-01-29 18:09:27 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -117,21 +117,7 @@ def game_hash
   }
 end 
 
-def good_practices
-  game_hash.each do |location, team_data|
-    puts location
-      team_data.each do |attribute, data|
-
-        binding.pry
- 
-
-        data.each do |data_item|
-          puts data_item
-            binding.pry
-      end
-    end
-  end
-end
-
-good_practices
+def num_points_scored
+  
+end 
 
